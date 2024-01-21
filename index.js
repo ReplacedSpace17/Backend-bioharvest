c3941259fdcca77aedeb994f954f49b5d8f4dc34
ReplacedSpace17 <replacedspace17@gmail.com>
2024-01-21 00:15:57 -0600

    Config create account

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const cors = require('cors'); // Importa el middleware CORS
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
+const nodemailer = require('nodemailer');
 app.use(bodyParser.json({ limit: '20mb' })); // Ajusta el límite según tus necesidades
 app.use('/uploads', express.static('uploads'));
 
@@ -21,14 +21,63 @@ app.use(session({
   saveUninitialized: true
 }));
 
+// configuracion de funciones
+const { checkEmailExists, addUser, activateUser, updatePersonalInfo } = require('./CrearCuenta/functionAccountNew');
 
-// ---- add user
-app.post('/api/users/add', async (req, res) => {
-  const data = req.body;
+let codigoInfo = {};
 
+//-------------------------------------------------------------> Cuenta
+app.post('/api/validate-email/:email', (req, res) => {
+  const { email } = req.params;
+
+  checkEmailExists(req, res, email);
 });
 
+// agregar usuario
+app.post('/api/user', (req, res) => {
+const data = req.body;
+addUser(req, res, data);
+console.log("Agregando usuario");
+});
 
+//enviar codigo
+app.post('/api/validate-code/:email', (req, res) => {
+  const { email } = req.params;
+  // Llamada a la función enviarCode con la asignación de la variable global
+  enviarCode(email).then((info) => {
+    codigoInfo = info;
+    console.log('Código generado:', info);
+  }).catch((error) => {
+    console.error('Error al enviar el código:', error);
+    // Puedes manejar el error según tus necesidades
+  });
+});
+
+app.post('/api/activate-user/:email', (req, res) => {
+  const { email } = req.params;
+  const { code } = req.body;
+
+
+  if (Number(code) === Number(codigoInfo.code)) {
+    console.log("Codigo correcto");
+    activateUser(req, res, email);
+  } else {
+    console.log("Codigo incorrecto");
+  }
+  
+  
+});
+
+// actualizar fecha de nacimiento
+app.post('/api/final-new-user/:uid', (req, res) => {
+  const { uid } = req.params;
+  const {birthdate} = req.body;
+  const {avatarName} = req.body;
+  
+  updatePersonalInfo(req, res, birthdate, avatarName, uid);
+  
+  
+});
 
 
 ////////////////////////////////////////////////////////-----------------> Incidencias
@@ -58,11 +107,59 @@ app.post('/api/incidencias/create', async (req, res) => {
 
 */
 
+/* --- Funciones de Mail --- */
+async function enviarCode(email) {
+  // Configurar el transporte de correo
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'bioharvest.contact@gmail.com',
+      pass: 'sxgl odwe wnhi qoqy ',
+    },
+  });
+
+  // Generar un código aleatorio (puedes usar cualquier lógica que necesites)
+  const code = Math.floor(100000 + Math.random() * 900000); // Ejemplo: código de 6 dígitos
+
+  // Obtener la marca de tiempo actual en milisegundos
+  const timestamp = Date.now();
+
+  // Contenido del correo
+  const mailOptions = {
+    from: 'bioharvest.contact@gmail.com',
+    to: email,
+    subject: 'Código de Verificación',
+    text: `Tu código de verificación es: ${code}`,
+  };
+
+  try {
+    // Enviar el correo
+    const info = await transporter.sendMail(mailOptions);
+
+    // Puedes realizar acciones adicionales aquí, como guardar el código y la marca de tiempo en la base de datos asociado a la UID, etc.
+    console.log('Código enviado con éxito. Detalles:', info);
+
+    // Devolver el código generado y la marca de tiempo
+    return { code, timestamp };
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+    throw error;
+  }
+}
+
+
+async function sendCODE(email) {
+  enviarCode(email).then((code) => {
+    console.log('Código generado:', code);
+  }).catch((error) => {
+    console.error('Error:', error);
+  });
+}
 
 // Ruta de ejemplo
 app.get('/test', (req, res) => {
   res.send('¡Hola, mundo!');
-  console.log("enro");
+  console.log("Test");
 });
 
 // Inicia el servidor
